0931ef6ac40465d58ccffbcf5c3b36cc72cf4e41
sayedhossainup <up.sayedhossain@gmail.com>
2024-10-25 11:41:59 +0600

    Update anberwps.php

--- anberwps.php
+++ anberwps.php
@@ -7,17 +7,17 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              https://github.com/frahim
+ * @link              https://github.com/sayedhossainup/anber-wp-security/
  * @since             1.0.0
  * @package           Anber_wp_security
 
  * @wordpress-plugin
  * Plugin Name:       Anber Wp Security
- * Plugin URI:        https://github.com/frahim
+ * Plugin URI:        https://github.com/sayedhossainup/anber-wp-security/
  * Description:       A Security solution for Wordpress
  * Version:           1.0.0
  * Author:            Md Yeasir Arafat
- * Author URI:        https://github.com/frahim/
+ * Author URI:        https://github.com/sayedhossainup/
  * License:           GPLv3
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain:       anber-wp-security
